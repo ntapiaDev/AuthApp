b584e1da71110a0b364dc2b6f58f2ce3a5062b73
Nicolas TAPIA <ntapia.dev@gmail.com>
2023-08-22 14:54:32 +0200

    JWT dans un cookie

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -3,6 +3,7 @@ import { User } from './user';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import jwtDecode from 'jwt-decode';
+import { CookieService } from 'ngx-cookie-service';
 import {
   HttpClient,
   HttpHeaders,
@@ -16,7 +17,7 @@ export class AuthService {
   endpoint: string = 'http://localhost:4000/api';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
   currentUser = {};
-  constructor(private http: HttpClient, public router: Router) {}
+  constructor(private http: HttpClient, public router: Router, private cookieService: CookieService) {}
   // Sign-up
   signUp(user: User): Observable<any> {
     let api = `${this.endpoint}/register-user`;
@@ -27,7 +28,17 @@ export class AuthService {
     return this.http
       .post<any>(`${this.endpoint}/signin`, user)
       .subscribe((res: any) => {
-        localStorage.setItem('access_token', res.token);
+        // localStorage.setItem('access_token', res.token);
+
+        // const cookieName = 'access_token';
+        // const cookieValue = res.token;
+        // const expirationDate = new Date();
+        // expirationDate.setTime(expirationDate.getTime() + res.expiresIn);    
+        // Créer un cookie sécurisé avec l'attribut HttpOnly et Secure
+        // document.cookie = `${cookieName}=${cookieValue}; expires=${expirationDate.toUTCString()}; path=/; HttpOnly; Secure`;
+
+        this.cookieService.set('access_token', res.token);
+
         this.getUserProfile(res._id).subscribe((res) => {
           this.currentUser = res;
           this.router.navigate(['user-profile/' + res.data._id]);
@@ -35,17 +46,20 @@ export class AuthService {
       });
   }
   getToken() {
-    return localStorage.getItem('access_token');
+    // return localStorage.getItem('access_token');
+    return this.cookieService.get('access_token');
   }
   getCurrentId() {
-    let authToken = localStorage.getItem('access_token');
+    // let authToken = localStorage.getItem('access_token');
+    let authToken = this.cookieService.get('access_token');
     if (authToken) {
       const decodedToken: any = jwtDecode(authToken);
       return decodedToken.userId;
     } else return null;
   }
   get isLoggedIn(): boolean {
-    let authToken = localStorage.getItem('access_token');
+    // let authToken = localStorage.getItem('access_token');
+    let authToken = this.cookieService.get('access_token');
     if (authToken) {
       const decodedToken: any = jwtDecode(authToken);   
       const currentTime = Date.now() / 1000;
@@ -56,7 +70,8 @@ export class AuthService {
     return false;
   }
   doLogout() {
-    let removeToken = localStorage.removeItem('access_token');
+    // let removeToken = localStorage.removeItem('access_token');
+    let removeToken = this.cookieService.delete('access_token', '/');
     if (removeToken == null) {
       this.router.navigate(['log-in']);
     }
